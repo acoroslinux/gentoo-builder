09829ef840aa9e50f635be4a58d21a7322f36b51
Manuel <manuelsilvarosa@gmail.com>
2026-07-29 05:10:34 +0000

    portage & gnome: enable pulseaudio and sound USE flags for media-libs/libcanberra as required by gnome-control-center

--- core/portage_manager.py
+++ core/portage_manager.py
@@ -91,10 +91,11 @@ class PortageManager:
                     "sys-kernel/installkernel -doc -man\n"
                     "# PulseAudio ALSA plugin required by xfce4-pulseaudio-plugin\n"
                     "media-plugins/alsa-plugins pulseaudio\n"
-                    "# GNOME Mutter & Wayland & Samba requirements\n"
+                    "# GNOME Mutter & Wayland & Samba & libcanberra requirements\n"
                     "x11-base/xwayland libei\n"
                     "x11-wm/mutter wayland\n"
                     "net-fs/samba client\n"
+                    "media-libs/libcanberra pulseaudio sound\n"
                 )
 
             # dracut.conf.d is written here for reference but MUST also be called
